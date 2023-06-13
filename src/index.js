c7066b7c40f862bd61483f9e4d6b1ac4d30a69ff
Alexuber <alexuberbuber@gmail.com>
2023-06-13 15:23:08 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+emailjs.init('ocW54qITR1KAUa2tO');
+
+window.onload = function () {
+  document
+    .getElementById('my-form')
+    .addEventListener('submit', function (event) {
+      event.preventDefault();
+      const formData = new FormData(this);
+      console.log('🆑  formData:', formData);
+      this.contact_number.value = (Math.random() * 100000) | 0;
+      emailjs.sendForm('service_jrj4b3f', 'template_x1h6esg', this).then(
+        function () {
+          console.log('SUCCESS!');
+        },
+        function (error) {
+          console.log('FAILED...', error);
+        }
+      );
+    });
+};
