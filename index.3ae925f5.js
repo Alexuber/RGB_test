a578d4485c001363d0609a67b3ad6cad454346c9
Alexuber <114301134+Alexuber@users.noreply.github.com>
2023-06-13 12:27:31 +0000

    Deploying to gh-pages from @ Alexuber/RGB_test@06f8291d674fcef8fd2cf7da4e204197dd785db5 🚀

---
+++ index.3ae925f5.js
@@ -0,0 +1,2 @@
+emailjs.init("ocW54qITR1KAUa2tO"),window.onload=function(){document.getElementById("my-form").addEventListener("submit",(function(e){e.preventDefault(),this.contact_number.value=1e5*Math.random()|0,emailjs.sendForm("service_jrj4b3f","template_x1h6esg",this).then((function(){console.log("SUCCESS!")}),(function(e){console.log("FAILED...",e)}))}))};
+//# sourceMappingURL=index.3ae925f5.js.map
